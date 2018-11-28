678b220f9cb4dc8bf8d67abadfa9dda1f2ea7f4e
Pere Busquet <perebusquet87gmail.com>
2018-11-28 11:06:41 +0100

    html

--- starter-code/iteration1/e2e/app.e2e-spec.ts
+++ starter-code/iteration1/e2e/app.e2e-spec.ts
@@ -1,14 +1,11 @@
-import { LabAngularIntroductionPage } from './app.po';
-
-describe('lab-angular-introduction App', function() {
-  let page: LabAngularIntroductionPage;
-
-  beforeEach(() => {
-    page = new LabAngularIntroductionPage();
+import { AppPage } from './app.po';
+ describe('workspace-project App', () => {
+  let page: AppPage;
+   beforeEach(() => {
+    page = new AppPage();
   });
-
-  it('should display message saying app works', () => {
+   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to TEST-PROJECT!');
   });
 });
--- starter-code/iteration1/e2e/app.po.ts
+++ starter-code/iteration1/e2e/app.po.ts
@@ -1,11 +1,7 @@
-import { browser, element, by } from 'protractor';
 
-export class LabAngularIntroductionPage {
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
-
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
-  }
-}
